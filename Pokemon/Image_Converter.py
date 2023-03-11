72597ddaf7a3634085c892329ae7069978fa64b4
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-11 11:13:20 +0300

    Update Image_Converter.py

--- Pokemon/Image_Converter.py
+++ Pokemon/Image_Converter.py
@@ -13,14 +13,15 @@ print(image_folder,output_folder)
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
+# Loop through input image folder path
 for filename in os.listdir(image_folder):
+    # Open image
     img = Image.open(f'{image_folder}{filename}') 
+    # Split image path filename
     clean_name = os.path.splitext(filename)[0]
+    # Print image name
     print(clean_name)
+    # Save image in output folder as png file
     img.save(f'{output_folder}{clean_name}.png','png')
+    # Print verification of loop success
     print('Done and Dusted')
-    #loop through Pokedex
-
-    #convert images to png
-
-    #save to the new folder
