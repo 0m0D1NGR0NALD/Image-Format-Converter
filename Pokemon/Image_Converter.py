e692863cada02cb6c0674462823ca293e3a67fde
Omoding Ronald <97228745+0m0D1NGR0NALD@users.noreply.github.com>
2023-03-11 11:09:56 +0300

    Update Image_Converter.py

--- Pokemon/Image_Converter.py
+++ Pokemon/Image_Converter.py
@@ -4,11 +4,14 @@ import sys
 import os
 from PIL import Image
     
-    #grab first and second argument
+# Capture first and second argument
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
-    # check if new/ exists, if not create
+
+# Print input and output image folder paths
 print(image_folder,output_folder)
+
+# Check if output folder exists, if not create output folder
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
